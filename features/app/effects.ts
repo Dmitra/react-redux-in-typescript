efc642e16052ed9102f8d6956b5142ee14badc50
Adrian Dmitra <adrian@dmitra.com>
2021-12-01 15:56:22 +0300

    comments on missing types

--- features/app/effects.ts
+++ features/app/effects.ts
@@ -8,6 +8,7 @@ export function* onBoot () {
   while (true) {
     yield take(App.actions.boot.type)
     const batch = []
+    // App.actions.toggleLoading has wrong param type
     batch.push(App.actions.toggleLoading('APP'))
     batch.push(Doc.actions.load({ id: 'foo' }))
 
